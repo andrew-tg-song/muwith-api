7cc2d7a1615a3344e9bde21645e90a54caca962d
Andrew Song <ontoro339@gmail.com>
2024-01-21 19:02:39 +0900

    Remove unnecessary comment

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('USER')
 export class User {
-  @PrimaryGeneratedColumn('increment') // 자동으로 증가되는 옵션 다른 옵션도 있다.
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
   @Column()
